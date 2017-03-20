17622a03e59e765c6e04549a7ce70b123a66300b
miller <miller_maxwell@outlook.com>
2017-03-20 20:29:07 +0800

    leetcode 383

---
+++ 453-Minimum_Moves_to_Euqal_Array_Element/main.py
@@ -0,0 +1,5 @@
+def minMoves(nums):
+    return sum(nums) - len(nums) * min(nums)
+
+if __name__ == '__main__':
+    print minMoves([1,2,3])
--- utils.cpp
+++ utils.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
+
+//打印向量
 template <class T>
 void print(vector<T> vec){
+  for(auto& iter:vec){
+    cout << iter << ' ';
+  }
+  //刷新缓冲区
+  cout << endl;
+}
+
+//打印数组
+template <class T, size_t n>
+void print(array<T,n> vec){
   for(auto& iter:vec){
     cout << iter << ' ';
   }
